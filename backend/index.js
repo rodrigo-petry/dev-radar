a08cc013cf9f0d6a6ab55f9a2beb91c583e916ce
Rodrigo Petry <rodrigo.petry18@gmail.com>
2020-01-23 22:29:21 -0300

    Configurações de Conexão com Banco de Dados

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://rodrigo:gqwabc123@rodrigo-jrnk3.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello Omnistack' });

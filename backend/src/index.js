20250682692cab8f3c56488d781df252e9954d53
Rodrigo Petry <rodrigo.petry18@gmail.com>
2020-01-24 14:53:55 -0300

    Controller de Criação de Desenvolvedor

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://rodrigo:gqwabc123@rodrigo-jrnk3.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Omnistack' });
-});
+app.use(routes);
 
 app.listen(3333);

444f3397e7bb637e20b2d9b3646ead4f37f66369
Rodrigo Petry <rodrigo.petry18@gmail.com>
2020-01-28 14:35:59 -0300

    Configurando CORS para Consumo de Dados no React

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://rodrigo:gqwabc123@rodrigo-jrnk3.mongodb.net/week
   useFindAndModify: false
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
